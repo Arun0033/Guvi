1d21e2f22f8ae998257d84a57149aafb1e975c3d
Arun0033 <50897922+Arun0033@users.noreply.github.com>
2019-05-24 00:16:17 -0700

    Update vowelconstant.py

--- vowelconstant.py
+++ vowelconstant.py
@@ -1,8 +1,8 @@
 m=input("")
 if(m>='a' and m<='z' or m>='A' and m<='Z'):
  if(m=='a' or m=='e' or m=="i" or m=='o' or m=='u' or  m=='A' or m=='E' or m=='I' or m=='O' or  m=="U"):
-   print("vowel")
+   print("Vowel")
  else:
-  print("constant")
+  print("Constant")
 else:
   print("invalid")
