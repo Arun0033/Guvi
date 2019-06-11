d3029fa11055e3530452893a9b4165a7df909a4b
Arun0033 <50897922+Arun0033@users.noreply.github.com>
2019-06-11 06:08:11 -0700

    Create string has vowels.py

---
+++ string has vowels.py
@@ -0,0 +1,17 @@
+m=input()
+
+d=0
+
+for i in m:
+
+     if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+
+         d=1
+
+if(d==1):
+
+       print("yes")
+
+else:
+
+       print("no")
